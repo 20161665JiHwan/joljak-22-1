7b6ac1cddcbdae550ae08f6ee35995ee0927dca5
20161665JiHwan <20161665@vision.hoseo.edu>
2022-03-26 13:22:50 +0900

    상호작용 깜빡이 이펙트 적용

--- Source/MOD/Components/Triggers/InteractionTriggerComponent.h
+++ Source/MOD/Components/Triggers/InteractionTriggerComponent.h
@@ -13,6 +13,14 @@ class MANSIONOFDARKNESS_API UInteractionTriggerComponent : public UTriggerCompon
 protected:
 	virtual void BeginPlay() override;
 
+private:
+	UPROPERTY(EditAnywhere, Category = Trigger, meta = (AllowPrivateAccess = "true"))
+		class UMaterialInterface* interactMaterial;
+
+	TArray<class UStaticMeshComponent*> highlights;
+
+	void SetHighlightVisibility(bool visibility);
+
 public:
 	UFUNCTION()
 		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
--- Source/MOD/Controllers/PlayerCharacterController.cpp
+++ Source/MOD/Controllers/PlayerCharacterController.cpp
@@ -5,24 +5,3 @@ void APlayerCharacterController::OnPossess(APawn* aPawn)
 	Super::OnPossess(aPawn);
 
 }
-
-void APlayerCharacterController::PopupWidget()
-{
-	if (Widget_MainClass != nullptr)
-	{
-		Widget_Main = CreateWidget<UUserWidget>(this, Widget_MainClass);
-
-		if (Widget_Main != nullptr)
-		{
-			Widget_Main->AddToViewport();
-		}
-	}
-}
-
-void APlayerCharacterController::PushWidget()
-{
-	if (Widget_Main != nullptr)
-	{
-		Widget_Main->RemoveFromViewport();
-	}
-}
