7ac705be17c97137166894d88f51b7ecdff4b80d
20161665JiHwan <20161665@vision.hoseo.edu>
2022-04-06 17:15:42 +0900

    QTE 시스템 거의 구현
    캐릭터와의 상호작용은 구현되지 않음

--- Source/MOD/TrapInfo.h
+++ Source/MOD/TrapInfo.h
@@ -1,16 +1,22 @@
 #pragma once
 
-#include "CoreMinimal.h"
+//#include "CoreMinimal.h"
 #include "TrapInfo.generated.h"
 
 USTRUCT(Atomic, BlueprintType)
-struct MANSIONOFDARKNESS_API FTrapInfo
+struct FTrapInfo
 {
 	GENERATED_BODY()
-public:
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Event)
-		FName attribute;
+		FString message;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Event)
+		FString attribute;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Event, meta = (Min = 0))
 		int tryCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Event)
+		int damage;
 };
