4cbce2cca47145ef6333f7d0690aab8b64b01c33
20161665JiHwan <20161665@vision.hoseo.edu>
2022-04-18 03:55:09 +0900

    아이템 확인 추가

---
+++ Source/MOD/Components/Triggers/CollectionTriggerComponent.cpp
@@ -0,0 +1,35 @@
+#include "CollectionTriggerComponent.h"
+#include "MOD/MansionOfDarkness.h"
+
+#include "Kismet/GameplayStatics.h"
+
+#include "MOD/MODCharacter.h"
+#include "MOD/Inventory/InventoryComponent.h"
+
+void UCollectionTriggerComponent::Activate(bool bReset)
+{
+	Super::Activate(bReset);
+
+	UE_LOG(TriggerEvent, Log, TEXT("Collection Activate!"));
+
+	AMODCharacter* player = Cast<AMODCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	player->GetInventory()->OnInventoryUpdated.AddDynamic(this, &UCollectionTriggerComponent::CheckCollection);
+	CheckCollection();
+}
+
+void UCollectionTriggerComponent::Deactivate()
+{
+	Super::Deactivate();
+
+	AMODCharacter* player = Cast<AMODCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	player->GetInventory()->OnInventoryUpdated.Remove(this, FName("CheckCollection"));
+}
+
+void UCollectionTriggerComponent::CheckCollection()
+{
+	UE_LOG(TriggerEvent, Log, TEXT("collection check!"));
+	AMODCharacter* player = Cast<AMODCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	player->GetInventory()->OnInventoryUpdated.Remove(this, FName("CheckCollection"));
+	SetTrigger(player->GetInventory()->FindCollectionItem(itemName));
+
+}
---
+++ Source/MOD/Components/Triggers/CollectionTriggerComponent.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TriggerComponent.h"
+#include "CollectionTriggerComponent.generated.h"
+
+UCLASS(Blueprintable, ClassGroup = (Trigger))
+class MANSIONOFDARKNESS_API UCollectionTriggerComponent : public UTriggerComponent
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Activate(bool bReset = false) override;
+	virtual void Deactivate() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = TriggerP)
+		FText itemName;
+
+private:
+	UFUNCTION()
+	void CheckCollection();
+};
